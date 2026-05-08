fa80a45ed912f8770fefe33f1858d61f20ddbc4a
SotaSugiyama <shanshansatai8@gmail.com>
2026-05-08 14:53:51 +0900

    実装

--- project/engine/graphics/rendering/pso/PSOManager.cpp
+++ project/engine/graphics/rendering/pso/PSOManager.cpp
@@ -60,7 +60,7 @@ void PSOManager::CreateRootSignature(ID3D12Device* device)
 
     D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
     descriptorRange[0].BaseShaderRegister = 0;
-    descriptorRange[0].NumDescriptors = 1;
+    descriptorRange[0].NumDescriptors = 2;
     descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
     descriptorRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
 
