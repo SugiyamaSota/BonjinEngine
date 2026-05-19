47f4768a9ed047c80e12e9f88ae0b8a6d8b7f350
SotaSugiyama <shanshansatai8@gmail.com>
2026-05-19 13:47:58 +0900

    確認課題

--- project/resources/shader/FullScreen.PS.hlsl
+++ project/resources/shader/FullScreen.PS.hlsl
@@ -1,4 +1,4 @@
-#include "CopyImage.hlsli"
+#include "FullScreen.hlsli"
 
 Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
@@ -12,5 +12,7 @@ PixelShaderOutPut main(VertexShaderOutPut input)
 {
     PixelShaderOutPut output;
     output.color = gTexture.Sample(gSampler, input.texcoord);
+    float32_t value = dot(output.color.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
+    output.color.rgb = float32_t3(value, value, value);
     return output;
 }
